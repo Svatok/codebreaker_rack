9591b30ee15bf1095120ab059b3cfe763f5d1c9c
Svatok <s_s_s@ua.fm>
2016-12-11 22:38:35 +0200

    working without exir, save, restart

--- app/racker.rb
+++ app/racker.rb
@@ -1,5 +1,6 @@
 require_relative 'svatok_codebreaker.rb'
 require 'erb'
+require 'uri'
 
 class Racker
   def self.call(env)
@@ -14,17 +15,23 @@ class Racker
     @game_msg = @request.session[:game_msg]
     @app_message = SvatokCodebreaker::GameMessage.new
     @game = @request.session[:game]
+    @request.session[:guess] = @request.params['guess'] unless @request.params['guess'].nil?
   end
 
   def response
+    @path = @request.path
     case @request.path
-    when "/" then Rack::Response.new(render("index.html.erb"))
-    when "/game" then run_game
-    when "/hint" then command_hint
-    when "/exit" then command_exit
-    when "/restart" then command_restart
-    when "/save" then command_save
-    else Rack::Response.new("404", 404)
+      when "/" then Rack::Response.new(render("index.html.erb"))
+      when "/game" then run_game
+      when "/hint" then
+        @request.session[:command] = 'hint'
+        redirect_to('game')
+      when "/exit" then command_exit
+      when "/restart" then #command_restart
+        @request.session[:command] = 'restart'
+        redirect_to('game')
+      when "/save" then command_save
+      else Rack::Response.new("404", 404)
     end
   end
 
@@ -34,22 +41,22 @@ class Racker
   end
 
   def run_game
-    if @game.nil?
-      @request.session[:game] = SvatokCodebreaker::Game.new(@request.params['codebreaker_name'])
-      @request.session[:exit_game] = false
-      @game = @request.session[:game]
-    else
-      game_step
-    end
-    @request.session[:answers] = @answers
+    @game.nil? ? start_game : game_step
     Rack::Response.new(render("game.html.erb"))
   end
 
+  def start_game
+    @request.session[:game] = SvatokCodebreaker::Game.new(@request.params['codebreaker_name'])
+    @request.session[:exit_game] = false
+    @game = @request.session[:game]
+  end
+
   def game_step
-    return redirect_to if @exit_game
-    answer = @request.params['guess']
-    @game_msg = @app_message.show(:not_valid_answer) unless @game.valid_guess?(answer)
-    @game.submit_guess(answer) if @game.valid_guess?(answer)
+    redirect_to if @exit_game
+    answer = @request.params['guess'].nil? ? @request.session[:command] : @request.session[:guess]
+    return @game_msg = @app_message.show(:not_valid_answer) unless answer_valid?(answer)
+    return send(('command_' + answer.downcase).to_sym) if menu_command?(answer)
+    @game.submit_guess(answer)
     @game_msg = @game.end_of_game? ? game_completition : @app_message.show(:next_step, @game.get_game_data)
   end
 
@@ -58,9 +65,16 @@ class Racker
     @app_message.show(:lose, @game.get_game_data)
   end
 
+  def answer_valid?(answer)
+    @game.valid_guess?(answer) || menu_command?(answer)
+  end
+
+  def menu_command?(answer)
+    @request.session[:menu_commands].include?(answer)
+  end
+
   def command_hint
     @game_msg = @game.hint ? @game.show_hint : @app_message.show(:no_hint)
-    redirect_to('game')
   end
 
   def command_exit
@@ -77,6 +91,7 @@ class Racker
   def command_save
     @game_msg = @app_message.show(:not_available) unless @game.end_of_game?
     if @game.end_of_game?
+      @request.session[:command] = 'save'
       @game.save_game
       @game_msg = @app_message.show(:saved)
     end

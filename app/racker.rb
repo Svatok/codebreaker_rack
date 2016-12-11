3c32a87d62c441bc91f960e0141069d79b53c777
Svatok <s_s_s@ua.fm>
2016-12-12 00:11:41 +0200

    working game

--- app/racker.rb
+++ app/racker.rb
@@ -9,9 +9,8 @@ class Racker
 
   def initialize(env)
     @request = Rack::Request.new(env)
-    @exit_game = (@request.session[:exit_game] || @request.session[:exit_game].nil?)
+    @exit_game = @request.session[:exit_game].nil?
     @request.session[:menu_commands] ||= %w(exit restart hint save)
-    @request.session[:answers] ||= {}
     @game_msg = @request.session[:game_msg]
     @app_message = SvatokCodebreaker::GameMessage.new
     @game = @request.session[:game]
@@ -21,15 +20,13 @@ class Racker
   def response
     @path = @request.path
     case @request.path
-      when "/" then Rack::Response.new(render("index.html.erb"))
+      when "/" then
+        @request.session.clear
+        Rack::Response.new(render("index.html.erb"))
       when "/game" then run_game
-      when "/hint" then
-        @request.session[:command] = 'hint'
-        redirect_to('game')
+      when "/hint" then command_hint
       when "/exit" then command_exit
-      when "/restart" then #command_restart
-        @request.session[:command] = 'restart'
-        redirect_to('game')
+      when "/restart" then command_restart
       when "/save" then command_save
       else Rack::Response.new("404", 404)
     end
@@ -41,23 +38,29 @@ class Racker
   end
 
   def run_game
-    @game.nil? ? start_game : game_step
-    Rack::Response.new(render("game.html.erb"))
+    if @game.nil?
+      start_game
+    else
+      game_step
+      Rack::Response.new(render("game.html.erb"))
+    end
   end
 
   def start_game
+    return redirect_to if @request.params['codebreaker_name'].nil?
     @request.session[:game] = SvatokCodebreaker::Game.new(@request.params['codebreaker_name'])
     @request.session[:exit_game] = false
     @game = @request.session[:game]
+    @game_msg = @app_message.show(:start)
+    Rack::Response.new(render("game.html.erb"))
   end
 
   def game_step
-    redirect_to if @exit_game
-    answer = @request.params['guess'].nil? ? @request.session[:command] : @request.session[:guess]
-    return @game_msg = @app_message.show(:not_valid_answer) unless answer_valid?(answer)
-    return send(('command_' + answer.downcase).to_sym) if menu_command?(answer)
+    answer = @request.params['guess']
+    return if answer.nil?
+    return @game_msg = @app_message.show(:not_valid_answer) unless @game.valid_guess?(answer)
     @game.submit_guess(answer)
-    @game_msg = @game.end_of_game? ? game_completition : @app_message.show(:next_step, @game.get_game_data)
+    @game_msg = @app_message.show(:next_step) unless @game.end_of_game?
   end
 
   def game_completition
@@ -65,24 +68,17 @@ class Racker
     @app_message.show(:lose, @game.get_game_data)
   end
 
-  def answer_valid?(answer)
-    @game.valid_guess?(answer) || menu_command?(answer)
-  end
-
-  def menu_command?(answer)
-    @request.session[:menu_commands].include?(answer)
-  end
-
   def command_hint
-    @game_msg = @game.hint ? @game.show_hint : @app_message.show(:no_hint)
+    @game_msg = @game.hint ? 'Hint: ' + @game.show_hint : @app_message.show(:no_hint)
+    redirect_to('game')
   end
 
   def command_exit
-    @exit_game = true
     redirect_to
   end
 
   def command_restart
+    @request.session.clear
     @request.session[:game] = SvatokCodebreaker::Game.new(@game.instance_variable_get(:@player_name))
     @game_msg = @app_message.show(:restart_game)
     redirect_to('game')
@@ -99,11 +95,10 @@ class Racker
   end
 
   def redirect_to(path = '')
-    @request.session[:game_msg] = @game_msg
+    @request.session[:game_msg] = @game_msg unless path == ''
     Rack::Response.new do |response|
       response.redirect('/' + path)
     end
   end
-
 
 end
--- app/svatok_codebreaker.rb
+++ app/svatok_codebreaker.rb
@@ -1,5 +1,3 @@
-require_relative 'svatok_codebreaker/version'
-require_relative 'svatok_codebreaker/console_game'
 require_relative 'svatok_codebreaker/game'
 require_relative 'svatok_codebreaker/marker'
 require_relative 'svatok_codebreaker/game_message'
